83a62b23ce208d8abaca9044dce7a9b452c263c7
MariaGaOli <mariaclaragaoli@gmail.com>
2025-11-15 21:10:59 -0300

    Adicionando formulário

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Consultas from "./pages/Consultas";
 
-export default function App() {
-  return <AppRoutes />;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/consultas/*" element={<Consultas />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
